3f3abab2177a663902ea0640e7cb658a0d7f239e
Hamza Kamran <hamzakamrn8@gmail.com>
2026-02-16 12:24:01 +0500

    url updated in cors

--- server/server.ts
+++ server/server.ts
@@ -22,7 +22,11 @@ const app = express();
 
 app.use(
   cors({
-    origin: ["http://localhost:5173", "http://localhost:3000"],
+    origin: [
+      "http://localhost:5173",
+      "http://localhost:3000",
+      "https://thumbility.vercel.app",
+    ],
     credentials: true,
   }),
 );
