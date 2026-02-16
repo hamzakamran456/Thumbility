54c232999f2902aaec53653db7bbad8460033052
Hamza Kamran <hamzakamrn8@gmail.com>
2026-02-16 12:38:22 +0500

    url new set

--- server/server.ts
+++ server/server.ts
@@ -25,7 +25,7 @@ app.use(
     origin: [
       "http://localhost:5173",
       "http://localhost:3000",
-      "https://thumbility.vercel.app",
+      "https://thumbility-nine.vercel.app",
     ],
     credentials: true,
   }),
